c192f1d3c8c000778a6c8da55fa40ebd830085e8
Rohan Chandrakant Khadke <khadkerohan20@gmail.com>
2026-03-05 14:56:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add shadow to navbar on scroll
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.style.boxShadow = "0 4px 6px -1px rgba(0, 0, 0, 0.5)";
-    } else {
-        navbar.style.boxShadow = "none";
-    }
-});
